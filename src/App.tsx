7f1dadd5758d25b54572b078a9791b6deaed8eb7
Victor Garcia Mestre <vgmestre@gmail.com>
2023-07-26 10:37:21 +0200

    fix types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Suspense, lazy } from 'react';
 import './App.css';
 import Router from './Router';
 import HomePage from './pages/Home';
-import AboutPage from './pages/About';
 import SearchPage from './pages/Search';
 import Page404 from './pages/404';
 import Route from './Route';
+
+const AboutPage = lazy(() => import('./pages/About.tsx'));
 
 const routes = [
   {
@@ -16,10 +18,12 @@ const routes = [
 function App() {
   return (
     <main>
-      <Router routes={routes} defaultComponent={Page404}>
-        <Route path="/" Component={HomePage} />
-        <Route path="/about" Component={AboutPage} />
-      </Router>
+      <Suspense fallback={null}>
+        <Router routes={routes} defaultComponent={Page404}>
+          <Route path="/" Component={HomePage} />
+          <Route path="/about" Component={AboutPage} />
+        </Router>
+      </Suspense>
     </main>
   );
 }
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,14 @@
+import { LazyExoticComponent } from 'react';
+
 export type RouteParams = { query: string };
 
 export interface ComponentDefaultProps {
   routeParams: RouteParams;
 }
 
+type RouteComponent = (props: ComponentDefaultProps) => JSX.Element;
+
 export interface RouteProps {
   path: string;
-  Component: (props: ComponentDefaultProps) => JSX.Element;
+  Component: RouteComponent | LazyExoticComponent<RouteComponent>;
 }
