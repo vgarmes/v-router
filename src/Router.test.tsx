69f299f77d5cf48a9ff162ca34f4397253caab62
Victor Garcia Mestre <vgmestre@gmail.com>
2023-07-26 11:28:47 +0200

    link test

--- src/Router.test.tsx
+++ src/Router.test.tsx
@@ -1,8 +1,9 @@
 import { describe, it, expect, beforeEach, vi, Mock } from 'vitest';
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import Router from './Router';
 import { getCurrentPath } from './utils';
 import Route from './Route';
+import Link from './Link';
 
 vi.mock('./utils.ts', () => ({
   getCurrentPath: vi.fn(),
@@ -31,6 +32,30 @@ describe('Router', () => {
         <Route path="/" Component={() => <h1>Home</h1>} />
       </Router>
     );
+    expect(screen.getByText('About')).toBeTruthy();
+  });
+
+  it('should navigate using Link component', () => {
+    (getCurrentPath as Mock).mockReturnValueOnce('/');
+
+    const Home = () => (
+      <>
+        <h1>Home</h1>
+        <Link data-testid="about-link" to="/about">
+          Go to about
+        </Link>
+      </>
+    );
+
+    render(
+      <Router defaultComponent={() => <h1>404</h1>}>
+        <Route path="/about" Component={() => <h1>About</h1>} />
+        <Route path="/" Component={Home} />
+      </Router>
+    );
+
+    const link = screen.getByTestId('about-link');
+    fireEvent.click(link);
     expect(screen.getByText('About')).toBeTruthy();
   });
 });
