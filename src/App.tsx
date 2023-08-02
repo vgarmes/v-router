3f6da8779d7ae54cce36c740cbd4e270571bcbe8
Victor Garcia Mestre <vgmestre@gmail.com>
2023-08-02 15:36:40 +0200

    remove base

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Suspense, lazy } from 'react';
 import { Router, Route } from './';
 import HomePage from './pages/Home';
 import Page404 from './pages/404';
-import { base } from './app.config';
 
 const UserPage = lazy(() => import('./pages/User'));
 const AboutPage = lazy(() => import('./pages/About'));
@@ -18,7 +17,7 @@ function App() {
   return (
     <main>
       <Suspense fallback={null}>
-        <Router routes={routes} defaultElement={<Page404 />} basename={base}>
+        <Router routes={routes} defaultElement={<Page404 />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
         </Router>
