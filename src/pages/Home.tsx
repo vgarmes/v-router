aa5e3b9d20a9a6e9a7b324e08fe461609fb1c83a
Victor Garcia Mestre <vgmestre@gmail.com>
2023-08-03 23:47:44 +0200

    add links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,7 +38,7 @@ function HomePage() {
       >
         <Link to="/about">Go to About page</Link>
         <Link to={{ pathname: '/user/:id', pathSegments: { id: '123' } }}>
-          Go to User page with dynamic route (lazy loaded)
+          Go to User page with dynamic route
         </Link>
         <Link to="/lazy">Lazy loaded page</Link>
       </div>
--- src/pages/Lazy.tsx
+++ src/pages/Lazy.tsx
@@ -1,7 +1,14 @@
+import { Link } from '../Link';
+
 function LazyPage() {
   return (
     <div>
       <h1>I'm a lazy loaded page</h1>
+      <p>
+        You might have noticed a momentary blink since this page is lazy loaded
+        and it requested the necessary JavaScript from the server
+      </p>
+      <Link to="/">Go Home</Link>
     </div>
   );
 }
